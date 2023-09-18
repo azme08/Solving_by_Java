3ef492ee471fe9dd8775034cfdd37ffb10d609aa
Abdullah Al Azme <115527810+azme08@users.noreply.github.com>
2023-09-18 21:33:37 +0600

    Create Array_Code1.java

---
+++ Array_Code1.java
@@ -0,0 +1,17 @@
+//Array Declaration and finding a value in array
+public class Practice1 {
+
+    public static void main(String[] args){
+   
+       int[] arr=new int[5];
+       
+       arr[0]=1;
+       arr[1]=4;
+       arr[2]=6;
+       arr[3]=8;
+       arr[4]=10;
+       
+        System.out.println(arr[4]); //just check a value
+    }
+   
+}

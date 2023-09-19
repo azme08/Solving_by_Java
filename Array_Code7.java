517e251b041b939cc6e7dccad67bd479ba86b228
Abdullah Al Azme <115527810+azme08@users.noreply.github.com>
2023-09-19 21:35:56 +0600

    Create Array_Code7.java

---
+++ Array_Code7.java
@@ -0,0 +1,16 @@
+//Using For each loop in java
+public class Practice1 {
+
+    public static void main(String[] args){
+    
+       String[] arr={"Azme","Naim","Sakib","Neyamot"};
+       
+       for(String x: arr){
+           System.out.println(x);
+       }
+       
+       
+        
+        
+    }
+   

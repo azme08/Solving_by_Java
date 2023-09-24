394a32bf9b339bcdc1454064c86147a1eab32275
Abdullah Al Azme <115527810+azme08@users.noreply.github.com>
2023-09-24 20:09:52 +0600

    Array_Matrix5.java

---
+++ Array_Code14.java
@@ -0,0 +1,30 @@
+//Array Matrix simple code step to step
+
+public class Practice1 {
+
+    public static void main(String[] args){
+      
+        int[][] arr =new int[4][];
+        
+        arr[0]=new int[1];
+        arr[1]=new int[2];
+        arr[2]=new int[3];
+        arr[3]=new int[4];
+        int k=0;
+        //using for loop to maintain sequences
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i+1; j++) {
+                arr[i][j]=k;
+                k++;
+            }
+        }
+      //printing output
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(" "+arr[i][j]);
+            }
+             System.out.println("");
+        }
+    }
+   
+}

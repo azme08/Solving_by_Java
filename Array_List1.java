e8e52ba23fc74d2bc231c2b481e5c07566c5ba3a
Abdullah Al Azme <115527810+azme08@users.noreply.github.com>
2023-09-24 23:36:47 +0600

    Array_List1.java

---
+++ Array_List1.java
@@ -0,0 +1,20 @@
+//Basic Array List Program
+import java.util.ArrayList;
+
+public class Practice1 {
+
+    public static void main(String[] args){
+      
+        ArrayList<Integer> arr=new ArrayList<>();
+        
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        
+        System.out.println("Array List Contains: "+arr);
+        System.out.println("Size Of Array: "+arr.size());
+
+    }
+   
+}
